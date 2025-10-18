97b01279d1d1e4223331fe87981350e2b02a3b98
OskarMannik <mannikoskar@gmail.com>
2025-10-18 09:42:09 +0300

    data exchange between worker and api part (test) #19

--- src/api/routes/discovery.py
+++ src/api/routes/discovery.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter, status
 import uuid
+from pydantic import BaseModel
+
 
 router = APIRouter(
     prefix="/discovery",
     tags=["Discovery"]
 )
+
+class DiscoveryResult(BaseModel):
+    assets: dict  
+    job_id: str
 
 @router.post("/", status_code=status.HTTP_202_ACCEPTED)
 def start_discovery_job():
@@ -22,3 +28,14 @@ def start_discovery_job():
     
     # Hetkel tagastame staatilise vastuse, mis kinnitab, et töö on vastu võetud.
     return {"message": "Asset discovery job has been started.", "job_id": job_id}
+
+
+@router.post("/results", status_code=status.HTTP_200_OK)
+def receive_discovery_results(data: DiscoveryResult):
+    """
+    Saab workerilt ansible taski data kujul ({"assets": [...], "job_id": "..."}).
+    """
+    print(f"Received discovery results for job {data.job_id}: {data.assets}")
+    
+    
+    return {"message": "Discovery results received successfully", "job_id": data.job_id}
